daeeb1268aeea9e6507d5f8f2d541583d10bda8e
rertzer <rertzer@student.42.fr>
2024-02-12 15:08:37 +0100

    day00 ex05

---
+++ python_00/ex05/building.py
@@ -0,0 +1,50 @@
+import sys
+
+def text_analyser(text = None):
+    if not text:
+       print("What is the text to count?")
+       for line in  sys.stdin:
+           text = line
+           break
+    if not isinstance(text, str):
+        print("ERROR (argument is not a string)")
+        sys.exit()
+    print("The text contains", len(text), "characters:")
+    up = 0
+    lw = 0
+    pu = 0
+    sp = 0
+    dg = 0
+    for x in text:
+        if x.isupper():
+            up += 1
+        elif x.islower():
+            lw += 1
+        elif x.isspace():
+            sp += 1
+        elif x.isdigit():
+            dg += 1
+        elif x in ('.', ',', ';', ':', '?', '!'):
+            pu += 1
+    print(up, " upper letters")
+    print(lw, " lower letters")
+    print(pu, " punctuation marks")
+    print(sp, " spaces")
+    print(dg, " digits")
+
+def main():
+    """ Given a string as argument, count the number of upper letters, lower letters, 
+    punctuation marks, spaces and digits. If no string is provided, reads on stdin.
+    In this case the final endline will unfortunatly be counted. This behavior can
+    easily be avoided, but expected by the subject :(
+    Using assert for parsing in strongly advised AGAINST. assert should be used only
+    for debug. The subject disagree ;("""
+    assert len(sys.argv) < 3, "to many arguments"
+
+    if len(sys.argv) == 1:
+        text_analyser(None)
+    else:
+        text_analyser(sys.argv[1])
+
+if __name__ == "__main__":
+    main()
